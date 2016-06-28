36d311b2d5106c3fbc3fe5b2470fb5224784904b
emilycharris <emily.harris.mail@gmail.com>
2016-06-28 00:08:24 -0400

    cross referenced urls

--- main.py
+++ main.py
@@ -28,8 +28,65 @@ def make_api_call(data):
         detail = input("Enter the number of the record you'd like to see: ")
         detail_url = url + "/" + detail
         response = requests.get(detail_url).json()
+
         for key, value in response.items():
+            if key == 'films' and key != None:
+                films = value
+                value_list = []
+                for film in films:
+                    response = requests.get(film).json()
+                    films = value_list.append(response['title'])
+                    value = value_list
+
+            elif key == 'species' and key != None:
+                species = value
+                value_list = []
+                for species in species:
+                    response = requests.get(species).json()
+                    species = value_list.append(response['name'])
+                    value = value_list
+
+            elif key == 'vehicles' and key != None:
+                vehicles = value
+                value_list = []
+                for vehicle in vehicles:
+                    response = requests.get(vehicle).json()
+                    vehicles = value_list.append(response['model'])
+                    value = value_list
+
+            elif key == 'starships' and key != None:
+                starships = value
+                value_list = []
+                for starship in starships:
+                    response = requests.get(starship).json()
+                    starships = value_list.append(response['name'])
+                    value = value_list
+
+            elif key == 'characters' and key != None:
+                characters = value
+                value_list = []
+                for character in characters:
+                    response = requests.get(character).json()
+                    characters = value_list.append(response['name'])
+                    value = value_list
+
+            elif key == 'planets' and key != None:
+                planets = value
+                value_list = []
+                for planet in planets:
+                    response = requests.get(planet).json()
+                    planets = value_list.append(response['name'])
+                    value = value_list
+
+            elif key == 'homeworld' and key != None:
+                planets = value
+                value_list = []
+                response = requests.get(planets).json()
+                planets = value_list.append(response['name'])
+                value = value_list
+
             print(key, ":", value, "\n")
+
 
 
 choice = int(input("""
